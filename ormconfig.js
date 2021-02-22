9bd4ccbd42773932a0bdcedbb10f7a39a230da5c
William Nunes <william.nunes@csghq.com>
2021-02-22 02:25:01 -0300

    Add docker and docker-compose files
    Add container files to run both the database and the server.
    Update .env.example file with enrivonment variables.

--- ormconfig.js
+++ ormconfig.js
@@ -3,6 +3,8 @@ module.exports = {
   host: `${process.env.DB_HOST}`,
   port: `${process.env.DB_PORT}`,
   database: `${process.env.DB_NAME}`,
+  username: `${process.env.DB_USER}`,
+  password: `${process.env.DB_PASS}`,
   useUnifiedTopology: true,
   entities: [
     './src/database/schemas/*.ts',
